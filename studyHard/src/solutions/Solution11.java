d8dcfb38d6f4da6ccea87748eab6d8764306c6cf
jihujung <jungjihu@gmail.com>
2023-09-01 23:02:28 +0900

    solution

---
+++ studyHard/src/solutions/Solution11.java
@@ -0,0 +1,97 @@
+package solutions;
+
+public class Solution11 {
+
+	public static void main(String[] args) {
+		//세로배열
+		for(int i=1; i<=9; i++) { //단수
+			for(int j=2; j<=9; j++) { //곱하기
+				if(i %2 == 0) { //짝수일때
+					System.out.print(i + " x " +  j + " = " +  ( i * j) + "\t");
+					}
+			}
+			System.out.println();
+		}
+		
+		//가로배열
+		//첫번째 시도
+//		for(int i=2; i<=9; i++) {
+//			if(i %2 == 0) {
+//				for(int j=1; j<=5; j++) {
+//					System.out.print(i + " x " +  j + " = " +  String.format("%2d", i * j) + "\t");
+//					
+//				}
+//				
+//			}else {
+//				for(int j=6; j<=9; j++) {
+//					System.out.printf(i + " x " +  j + " = " +  String.format("%2d", i * j) + "\t");
+//					
+//				}
+//				System.out.println();
+//			}
+//			
+//		}
+
+		//두번째 시도
+//		for(int j=1; j<=9; j++) { //곱하기
+//			for(int i=2; i<=9; i++) { //단수
+//				if(i%2 == 0 && j<=5) { //짝수일 때 1,2,3,4,5 곱셈만
+//					System.out.print(i + " x " +  j + " = " +  i * j + "\t");
+//				}else if(i%2 == 1) { //홀수일 때 6,7,8,9 곱셈만
+//					for(int a=3; a<=9; a+=2) { //곱하기
+//						if(a>=5) {
+//							System.out.print(i + " x " +  a + " = " +  i * a + "\t");
+//						    break;
+//						}else if(i==9) {
+//							System.out.println(i + " x " +  a + " = " +  i * a + "\t");
+//						}
+//					}
+//					
+//					
+//				}
+//			
+//				}
+//			
+//			
+//			}
+	
+		//세번째 시도
+//		for(int j=1; j<=9; j++) { //곱하기
+//			for(int i=2; i<=9; i++) { //단수
+//				switch(i) {
+//				case 2: case 4: case 6: case 8:
+//					if(j<=5) {
+//						System.out.print(i + " x " +  j + " = " +  i * j + "\t");
+//					}else if(j<=6) {
+//						
+//					}
+//					break;
+//				case 3: case 5: case 7:jj
+//					if(j<=5) {
+//						System.out.print(i + " x " +  (j+5) + " = " +  i * (j+5) + "\t");
+//					}else if(j>=6) {
+//						System.out.print(i + " x " +  j + " = " +  i * j + "\t");
+//					}
+//					break;	
+//				
+//				case 9:
+//					if(j<=5) {
+//						System.out.println(i + " x " +  (j+5) + " = " +  i * (j+5) + "\t");
+//					}else if(j>=6) {
+//						System.out.println(i + " x " +  j + " = " +  i * j + "\t");
+//					}
+//					break;	
+//				}
+//				
+//				}
+//			
+//				}
+		
+		
+		
+	}
+
+}
+
+
+
